5ff5666cdac17a1cec552c1ce0f88eb16b05e913
pskrzypek97 <tofanek9243@gmail.com>
2022-10-27 16:49:49 +0200

    Added SuccessModal component

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -1,6 +1,7 @@
 import Checkout from '../../components/Checkout/Checkout';
 import Summary from '../../components/Summary/Summary';
 import GoBackButton from '../../components/UI/GoBackButton';
+import SuccessModal from '../../components/Modal/SuccessModal';
 
 const CheckoutPage = () => {
 	return (
@@ -11,6 +12,7 @@ const CheckoutPage = () => {
 				<Checkout />
 				<Summary />
 			</section>
+			<SuccessModal />
 		</>
 	);
 };
